3f0790b328afe25d823471f1fe89b2cd6b370869
alexlf <alexlf@live.ca>
2024-03-01 19:22:32 -0700

    Adds better negative sample production and ploting for validation loss

--- Chen 2020 UKGE/train_model.py
+++ Chen 2020 UKGE/train_model.py
@@ -3,30 +3,66 @@ import torch
 from KGDataset import KGDataset
 from UKGE import UKGE
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+import numpy as np
+import random
 
 #setup
-epochs = 100
+epochs = 300
 learn_rate = 0.001
 dim = 128
-batch_size = 128
+batch_size = 512
 device = "cuda"
-data_dir = "./data/cn15k"
+data_dir = "./data/"
+data_set = "nl27k"
 
 #setup datasets
-train_set = KGDataset(data_dir + "/train.tsv")
-val_set = KGDataset(data_dir + "/val.tsv", train_set)
-#generate negative samples
-train_set.createNegativeSamples(2)
+train_set = KGDataset(data_dir + data_set + "/train.tsv")
+val_set = KGDataset(data_dir + data_set + "/val.tsv", train_set)
 
-#setup data laoders
+#setup data loaders
 train_loader = DataLoader(train_set, batch_size, shuffle=True)
 val_loader = DataLoader(val_set, batch_size, shuffle=True)
 
 #model setup
 model = UKGE(len(train_set.ents), len(train_set.rels), dim)
+
+#load model from ./model folder
+#model = torch.load("./models/9.09e-02.model")
 model.to(device)
+
+#setup loss function and optimizer
+#loss function is MSELoss, optimizer is Adam
+
 loss_func = torch.nn.MSELoss()
 optim = torch.optim.Adam(model.parameters(), learn_rate)
+
+def createNegativeSamples(dataset, relation_set, ratio):
+    with torch.no_grad():
+        dataset.triples = dataset.triples[:dataset.num_base].clone()
+        dataset.weights = dataset.weights[:dataset.num_base].clone()
+        dataset.resetTupleSet()
+        num_negatives = int(dataset.triples.size(0)*ratio)
+        created_negatives = []
+        weights = []
+        while len(created_negatives) < num_negatives:
+            #sample a random relationship and corrupt
+            #this is REAL bad, but it's what the paper does
+            rsh = random.randint(0, dataset.triples.size(0)-1)
+            if random.randint(0,1):
+                nrel = (dataset.triples[rsh,0].item(), dataset.triples[rsh,1].item(), random.randint(0, len(dataset.ents)-1))
+            else:
+                nrel = (random.randint(0, len(dataset.ents)-1), dataset.triples[rsh,1].item(), dataset.triples[rsh,2].item())
+            if nrel not in relation_set:
+                created_negatives.append(nrel)
+                weights.append(0.0)
+                dataset.triples_record.add(nrel)
+
+        #tensorize and cat negative examples
+        neg_t = torch.tensor(created_negatives, dtype=torch.int)
+        dataset.triples = torch.cat([dataset.triples, neg_t], 0)
+        dataset.weights = torch.cat([dataset.weights, torch.tensor(weights)], 0)
 
 #validation loss
 def eval():
@@ -37,11 +73,34 @@ def eval():
             loss += loss_func(confidences, targets.to(device)).detach()
     #output val loss and example prediction
     print(f"Val loss: {loss/len(val_loader):.2e} \nExample: ({val_set.lookup_ents[data[0,0].item()]}, {val_set.lookup_rels[data[0,1].item()]}, {val_set.lookup_ents[data[0,2].item()]}): Predicted: {confidences[0].item():.2f} - Truth: {targets[0]:.2f}")
+    return loss.item()/len(val_loader)
+
+def plot_loss():
+    #plot validation loss for each epoch
+    plt.plot(np.arange(0,len(val_record),1), val_record, label="Validation Loss")
+    plt.plot([0, len(val_record)-1], [0.0236, 0.0236], 'k--', lw=2, label="Reported Loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+
+    #plt.yscale('log')
+    #ax = plt.gca()
+    #ax.yaxis.set_major_formatter(ScalarFormatter())
+
+    plt.legend()
+    plt.title("Validation Loss over Epochs")
+    plt.savefig(f'{data_set}-val_loss.png', dpi=300)
+    plt.close()
 
 r_loss = 0
+best_loss = 10
+
+#keep track of validation loss each epoch
+val_record = []
 
 for e in range(epochs):
-    for (data, targets) in (pbar := tqdm(train_loader)):
+    #generate new negative samples
+    createNegativeSamples(train_set, train_set.triples_record, 2)
+    for i, (data, targets) in enumerate(pbar := tqdm(train_loader)):
         confidences = model(data.to(device))
         loss = loss_func(confidences, targets.to(device))
         loss.backward()
@@ -49,4 +108,9 @@ for e in range(epochs):
         optim.zero_grad()
         r_loss = r_loss * 0.9 + loss.detach() * 0.1
         pbar.set_description(f"e: {e} - t_loss: {r_loss:.2e}")
-    eval()
+    vloss = eval()
+    val_record.append(vloss)
+    plot_loss()
+    if vloss < best_loss:
+        torch.save(model, f"./models/{data_set}/{vloss:.2e}.model")
+        best_loss = vloss
